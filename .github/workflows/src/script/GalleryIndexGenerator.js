84c25fc2ee96cd05ce2d3e126a7ccc1a854ed7e3
ltws <ltws4815@gmail.com>
2026-09-07 17:46:25 +0900

    refactor: Move GitHub Actions workflow files

--- .github/workflows/src/script/GalleryIndexGenerator.js
+++ .github/workflows/src/script/GalleryIndexGenerator.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 try {
     let files = fs.readdirSync("resources/gallery/");
-    let regex = /\.(jpg|jpeg|png|gif|webp)$/i;
+    let regex = /\.(jpg|jpeg|png|gif|webp|svg)$/i;
     let index = files.filter(file => regex.test(file));
     fs.writeFileSync("resources/gallery/index.json", JSON.stringify(index, null, 2));
 } catch(error) {
