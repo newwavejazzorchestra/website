97fb7b6c01c9ffd9c2f79d3bf9f13bd2b22d5ee2
ltws <ltws4815@gmail.com>
2026-09-07 15:10:49 +0900

    fix: Fix GalleryIndexGenerator.js

--- .github/workflows/src/GalleryIndexGenerator.js
+++ .github/workflows/src/GalleryIndexGenerator.js
@@ -1,30 +1,11 @@
 const fs = require("fs");
-const token = process.env.GITHUB_TOKEN;
 
-fetch(
-    "https://api.github.com/repos/newwavejazzorchestra/website/contents/resources/gallery/",
-    {
-        headers: {
-            "Authorization": `token ${token}`,
-            "Accept": "application/vnd.github+json",
-            "User-Agent": "GitHub-Actions-Workflow-Step"
-        }
-    }
-)
-.then(response => {
-    if(response.ok) {
-        return response.json();   
-    } else {
-        return [];
-    }
-})
-.then(json => {
-    json = Array.isArray(json) ? json : [];
-
+try {
+    let files = fs.readdirSync("resources/gallery/");
     let regex = /\.(jpg|jpeg|png|gif|webp)$/i;
-    let index = json.filter(file => file.type == "file" && regex.test(file.name)).map(file => file.name);
+    let index = files.filter(file => regex.test(file));
     fs.writeFileSync("resources/gallery/index.json", JSON.stringify(index, null, 2));
-})
-.catch(error => {
+} catch(error) {
     console.error("Error has occured.", error);
-})
+    process.exit(1);
+}
